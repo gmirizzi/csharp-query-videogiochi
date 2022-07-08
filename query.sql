8e2cbad0505bbfa119eabdbb3eb5a424466de68e
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-08 17:08:39 +0200

    join query 2

--- query.sql
+++ query.sql
@@ -111,14 +111,14 @@ ON players.id = player_id
 GROUP BY player_id, name, lastname, nickname, city;
 -- 
 -- 2- Sezionare tutti i videogame dei tornei tenuti nel 2016, mostrandoli una sola volta (226)
-SELECT videogame_id
+SELECT videogame_id, videogames.name
 FROM videogames
 RIGHT JOIN tournament_videogame
 ON videogames.id=videogame_id
 LEFT JOIN tournaments
 ON tournament_id=tournaments.id
 WHERE year = 2016
-GROUP BY videogame_id;
+GROUP BY videogame_id, videogames.name;
 -- 
 -- 3- Mostrare le categorie di ogni videogioco
 -- SELECT v.id AS videogame_id, v.name AS videogame_name, v.release_date, c.id AS category_id, c.name AS category_name (1718)
